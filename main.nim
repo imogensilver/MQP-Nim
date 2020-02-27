26e838740da696ad02a4fbd84894f81d7ec148c5
Imogen Silver <imogensilver@statictypesoftware.com>
2020-02-27 12:57:31 -0500

    back to old tests. can now compute 5 ^ 2

--- main.nim
+++ main.nim
@@ -14,26 +14,24 @@ const
   initial_energy = (initial_functions.len ^ depth).float
   initial_preference_table = repeat(1.0 / initial_functions.len.float, initial_functions.len)
   ZERO = newLiteral(0.int, initial_energy, initial_preference_table, "0")
-  # x = 0...10
-  relation = proc(a: int): int = return a ^ 2
+  x = 0...10
 ##RUNNING TEST
 
-# for x, y in zip(x, x.map(relation)):
-#   >>> (x, y)
-#   let initial_literals = @[ZERO, newLiteral(x, initial_energy, initial_preference_table, "X[" & $x & "]")]
-#   >>> testTreeToDepth(initial_literals,
-#                     initial_functions,
-#                     max_depth,
-#                     true,
-#                     proc (a: Literal[type x]): bool =
-#                       a.X == y)
-const
-  x = 3
-  y = 9
-  initial_literals = @[ZERO, newLiteral(x, initial_energy, initial_preference_table, "X[" & $x & "]")]
-
->>> testTreeToDepth(initial_literals,
+for x, y in zip(x, x.map(proc(a: int): int = return a ^ 2)).items:
+  >>> (x, y)
+  let initial_literals = @[ZERO, newLiteral(x, initial_energy, initial_preference_table, "X[" & $x & "]")]
+  >>\ testTreeToDepth(initial_literals,
                     initial_functions,
-                    11,
+                    depth,
                     true,
                     proc (a: Literal[type x]): bool = a.X == y)
+# const
+#   x = 3
+#   y = 9
+#   initial_literals = @[ZERO, newLiteral(x, initial_energy, initial_preference_table, "X[" & $x & "]")]
+#
+# >>> testTreeToDepth(initial_literals,
+#                     initial_functions,
+#                     depth,
+#                     true,
+#                     proc (a: Literal[type x]): bool = a.X == y)
