f19be96a2ab99b160809795acc3923697b359d32
Imogen Silver <imogensilver@statictypesoftware.com>
2020-02-27 12:37:17 -0500

    added iterators and increased clarity

--- imogen_sugar/syntax.nim
+++ imogen_sugar/syntax.nim
@@ -1,3 +1,10 @@
+import ../framework/types
+
+#[
++ - * \ / < > = @ $ ~ & % ! ? ^ . |
+]#
+
+### PRINTERS
 template `>>>`*(a: untyped): untyped =
   echo a
 template `>>\`*(a: untyped): untyped =
@@ -11,9 +18,29 @@ template `>@\`*[T](a: seq[T]): untyped =
   >@> a
   echo "\n"
 
+### METHOD EQUALS CRUNCH
 template `.=`*[T](a: var T, b: proc(a: T): T): untyped =
   a = b(a)
 
+### RANGE OPERATOR
 proc `...`*(a, b: int): seq[int] =
   for i in a..b:
     result.add(i)
+
+
+### ITERATORS
+iterator energized_pairs*[T](a: seq[Literal[T]]): (int, Literal[T]) =
+  for index, item in a.pairs:
+    if(item.energy != -1):
+      yield (index, item)
+
+iterator menergized_pairs*[T](a: var seq[Literal[T]]): (int, var Literal[T]) =
+  for index, item in a.mpairs:
+    if(item.energy != -1):
+      yield (index, item)
+
+iterator `>..`*[T]( b: var seq[Literal[T]], a: int): var Literal[T] =
+  var i = a + 1
+  while i < b.len:
+    yield b[i]
+    i.inc
